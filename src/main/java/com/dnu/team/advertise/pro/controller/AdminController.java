bcfda3483a2757145a6c54e92f41c6a5dbfc0dbe
Karpov Ilya <karpov-illya@bigmir.net>
2015-12-26 11:18:55 +0200

    При удалении из таблицы "Услуги" проверка со связей с таблицой "Места"

--- src/main/java/com/dnu/team/advertise/pro/controller/AdminController.java
+++ src/main/java/com/dnu/team/advertise/pro/controller/AdminController.java
@@ -15,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
